eaf32d0d460fee8722c7e729ac6328edf17e1a0e
Benjamin Sloutsky <64224906+bslou@users.noreply.github.com>
2022-12-24 19:02:20 -0800

    debugged partners

--- pages/c/partners/index.js
+++ pages/c/partners/index.js
@@ -14,6 +14,7 @@ import {
 } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { db } from "../../api/firebaseconfig";
 import BottomNavBar from "../bottomnavbar";
 import NavBar from "../navbar";
 
