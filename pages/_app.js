217373398e9aeb57e551064c9f30dbf08c69f428
Benjamin Sloutsky <64224906+bslou@users.noreply.github.com>
2022-12-29 20:44:59 -0800

    updated logo

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,17 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Head>
-        <script
+        {/* <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7343484395424686"
           crossorigin="anonymous"
-        ></script>
+        ></script> */}
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="shortcut icon" href="favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon.ico" />
-        <link rel="icon" href="favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="logo.png" />
+        <link rel="icon" href="logo.png" />
         <link ref="preconnect" href="https://fonts.gstatic.com" />
       </Head>
       <SEO />
